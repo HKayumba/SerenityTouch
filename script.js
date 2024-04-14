08e2c16a8a8161fd8b0cb5a39a530f809cea6cfe
Honore Kayumba <84237371+HKayumba@users.noreply.github.com>
2024-04-15 01:08:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -29,23 +29,4 @@ window.addEventListener("load", function(){
   loader.style.display = "none"
 })
 
-// Page animation
-window.addEventListener('scroll', reveal);
-
-function reveal (){
-  var reveal = document.querySelectorAll('.reveal');
-
-  for(var i = 0; i < reveals.lenght; i++){
-    var windowheight = window.innerHeight;
-    var revealtop = reveals[i].getBoundingClientRect().top;
-    var revealpoint = 150;
-
-    if(revealtop < windowheight - revealpoint){
-      reveals[i].classList.add('active');
-    }
-    else{
-      reveals[i].classList.remove('active');
-    }
-  }
-}
 
